99b65b62cfdfd001e9a15feeb00a30365d17dddc
SwaraMansi <168108797+SwaraMansi@users.noreply.github.com>
2025-10-16 23:19:44 +0530

    Time: 20 ms (42.3%), Space: 45.4 MB (40.97%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.java
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -1,34 +1,41 @@
-class Solution {
-    public String minWindow(String s, String t) {
-        if (s == null || t == null || s.length() < t.length()) return "";
+import java.util.HashMap;
 
-        HashMap<Character, Integer> tFreq = new HashMap<>();
-        for (char c : t.toCharArray()) {
-            tFreq.put(c, tFreq.getOrDefault(c, 0) + 1);
+public class Solution {
+    public String minWindow(String s, String t) {
+        if (s == null || t == null || s.length() < t.length()) {
+            return "";
         }
 
-        int left = 0, right = 0, formed = 0;
-        int minLen = Integer.MAX_VALUE;
-        int minStart = 0;
+        HashMap<Character, Integer> tMap = new HashMap<>();
+        for (char c : t.toCharArray()) {
+            tMap.put(c, tMap.getOrDefault(c, 0) + 1);
+        }
+
+        int required = tMap.size();
+        int formed = 0;
         HashMap<Character, Integer> windowCounts = new HashMap<>();
+        int left = 0, right = 0;
+        int[] ans = {-1, 0, 0}; 
 
         while (right < s.length()) {
             char c = s.charAt(right);
             windowCounts.put(c, windowCounts.getOrDefault(c, 0) + 1);
 
-            if (tFreq.containsKey(c) && windowCounts.get(c).intValue() == tFreq.get(c).intValue()) {
+            if (tMap.containsKey(c) && windowCounts.get(c).intValue() == tMap.get(c).intValue()) {
                 formed++;
             }
 
-            while (formed == tFreq.size()) {
-                if (right - left + 1 < minLen) {
-                    minLen = right - left + 1;
-                    minStart = left;
+            while (left <= right && formed == required) {
+                if (ans[0] == -1 || right - left + 1 < ans[0]) {
+                    ans[0] = right - left + 1;
+                    ans[1] = left;
+                    ans[2] = right;
                 }
 
                 char leftChar = s.charAt(left);
                 windowCounts.put(leftChar, windowCounts.get(leftChar) - 1);
-                if (tFreq.containsKey(leftChar) && windowCounts.get(leftChar).intValue() < tFreq.get(leftChar).intValue()) {
+
+                if (tMap.containsKey(leftChar) && windowCounts.get(leftChar).intValue() < tMap.get(leftChar).intValue()) {
                     formed--;
                 }
 
@@ -38,6 +45,6 @@ class Solution {
             right++;
         }
 
-        return minLen == Integer.MAX_VALUE ? "" : s.substring(minStart, minStart + minLen);
+        return ans[0] == -1 ? "" : s.substring(ans[1], ans[2] + 1);
     }
 }
