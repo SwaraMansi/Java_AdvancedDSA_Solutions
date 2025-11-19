0fe1fe64454ae5d9e7b7483b3db019ef609c184e
SwaraMansi <168108797+SwaraMansi@users.noreply.github.com>
2025-11-19 22:40:26 +0530

    Time: 0 ms (100%), Space: 42.6 MB (25.82%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,23 +1,22 @@
 class Solution {
     public int rob(int[] nums) {
-       int n= nums.length;
-       int dp[] = new int[n+1];
-       Arrays.fill(dp,-1);
-       return recur(nums,dp,n); 
+        int n = nums.length;
+        int[] dp = new int[n];
+        Arrays.fill(dp, -1);
+
+        return recur(nums, dp, n - 1);
     }
-    private int recur(int[] nums , int[] dp, int index){
-        if(index==1){ 
-            dp[index]= nums[0];
-        return nums[0];
-        }
-        if(index==0){
-            dp[0]=0;
-            return 0;
-        }
-        if(dp[index] != -1) return dp[index];
-        int pick = nums[index-1] + recur(nums,dp,index-2);
-        int nopick= 0+ recur(nums,dp, index-1);
-        dp[index]= Math.max(pick,nopick);
-        return dp[index]; 
+
+    private int recur(int[] nums, int[] dp, int index) {
+        if (index == 0) return nums[0];
+        if (index < 0) return 0;
+
+        if (dp[index] != -1) return dp[index];
+
+        int pick = nums[index] + recur(nums, dp, index - 2);
+
+        int nopick = recur(nums, dp, index - 1);
+
+        return dp[index] = Math.max(pick, nopick);
     }
 }
