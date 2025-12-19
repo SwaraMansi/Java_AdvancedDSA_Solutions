27f0459157d019af5758b130c7e67221ac75e6df
SwaraMansi <168108797+SwaraMansi@users.noreply.github.com>
2025-12-19 16:49:07 +0530

    Time: 8 ms (45.66%), Space: 46.9 MB (37.61%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,15 +1,25 @@
 class Solution {
-    public int[] twoSum(int[] nums, int target) {
-       for(int left=0; left<nums.length-1;left++){
-       int right= nums.length-1;
-       while(right>left){
-        if(nums[left]+nums[right]==target){
-        return new int[]{left,right};
+    public int[] twoSum(int[] arr, int target) {
+      int n = arr.length;
+        int[][] numsWithIndex = new int[n][2];
+    
+        for (int i = 0; i < n; i++) {
+            numsWithIndex[i][0] = arr[i];
+            numsWithIndex[i][1] = i;
         }
-        else{
-            right--;
+        Arrays.sort(numsWithIndex, (a, b) -> Integer.compare(a[0], b[0]));
+
+        int left = 0, right = n - 1;
+        while (left < right) {
+            int sum = numsWithIndex[left][0] + numsWithIndex[right][0];
+            if (sum == target) {
+                return new int[] {numsWithIndex[left][1], numsWithIndex[right][1]};
+            } else if (sum < target) {
+                left++;
+            } else {
+                right--;
+            }
         }
-       }}
-       return new int[]{-1,-1};
+        return new int[] {-1, -1};
     }
 }
