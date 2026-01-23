c778b9e338855f5ab26635aaf243decd1ed70f17
SwaraMansi <168108797+SwaraMansi@users.noreply.github.com>
2026-01-23 12:48:01 +0530

    Time: 1 ms (72.89%), Space: 43.7 MB (39.32%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.java
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.java
@@ -18,7 +18,7 @@ class Solution {
       List<Integer> res = new ArrayList<>();
       if(root==null) return res;
       Queue<TreeNode> q= new LinkedList<>();
-     q.add(root);
+      q.add(root);
      while(!q.isEmpty()){
         int size= q.size();
         for(int i=0;i<size;i++){
