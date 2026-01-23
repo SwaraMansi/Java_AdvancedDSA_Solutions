e224b9c0f6e74a9db47e8948defde0378e0e092a
SwaraMansi <168108797+SwaraMansi@users.noreply.github.com>
2026-01-23 17:11:30 +0530

    Time: 0 ms (100%), Space: 49.7 MB (8.98%) - LeetHub

--- 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.java
+++ 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.java
@@ -1,24 +1,44 @@
-class TreeNode {
-    int val;
-    TreeNode left;
-    TreeNode right;
-    TreeNode(int val) { this.val = val; }
-}
-
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
     public int countNodes(TreeNode root) {
-             List<Integer> arr = new ArrayList<>();
-        postorder(root, arr);
-        return arr.size();
+     if(root==null) return 0;
+     int lh= leftheight(root);
+     int rh= rightheight(root);   
+     if(lh==rh){
+        return (1<<lh)-1;
+     }
+     else{
+        return 1+countNodes(root.left)+countNodes(root.right);
+     }
     }
-
-    private void postorder(TreeNode root, List<Integer> arr) {
-        if (root == null) {
-            return; 
+    int leftheight(TreeNode root){
+        int h=0;
+        while(root!=null){
+            h++;
+            root=root.left;
         }
-        postorder(root.left, arr);
-        arr.add(root.val);
-        postorder(root.right, arr);
-       
+        return h;
+    }
+    int rightheight(TreeNode root){
+        int h=0;
+        while(root!=null){
+            h++;
+            root=root.right;
+        }
+        return h;
     }
 }
