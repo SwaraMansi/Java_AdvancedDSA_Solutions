ddbebdbb6b05f535e1bcf99342018886519740ab
SwaraMansi <168108797+SwaraMansi@users.noreply.github.com>
2026-02-03 17:41:00 +0530

    Time: 6 ms (72.5%), Space: 44.9 MB (50.36%) - LeetHub

--- 0494-target-sum/0494-target-sum.java
+++ 0494-target-sum/0494-target-sum.java
@@ -1,22 +1,39 @@
 class Solution {
     public int findTargetSumWays(int[] nums, int target) {
-         int totalSum = 0;
-        for (int x : nums) totalSum += x;
+         int total = 0;
+        for (int x : nums) total += x;
 
-        if (totalSum < Math.abs(target) || (totalSum + target) % 2 != 0)
+        if ((total + target) % 2 != 0 || total < Math.abs(target)) 
             return 0;
 
-        int S = (totalSum + target) / 2;
+        int sum = (total + target) / 2;
 
-        int[] dp = new int[S + 1];
-        dp[0] = 1;   
+        return countSubsets(nums, sum);
+    }
 
-        for (int num : nums) {
-            for (int s = S; s >= num; s--) {
-                dp[s] += dp[s - num];
+    private int countSubsets(int[] nums, int sum) {
+        int n = nums.length;
+
+        int[][] dp = new int[n][sum + 1];
+
+        if (nums[0] == 0) dp[0][0] = 2; 
+        else dp[0][0] = 1;
+
+        if (nums[0] != 0 && nums[0] <= sum)
+            dp[0][nums[0]] = 1;
+
+        for (int i = 1; i < n; i++) {
+            for (int s = 0; s <= sum; s++) {
+
+                int notTake = dp[i - 1][s];
+
+                int take = 0;
+                if (nums[i] <= s) take = dp[i - 1][s - nums[i]];
+
+                dp[i][s] = notTake + take;
             }
         }
 
-        return dp[S];
+        return dp[n - 1][sum];
     }
 }
