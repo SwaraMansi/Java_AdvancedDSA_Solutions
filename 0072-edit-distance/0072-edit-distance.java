bcd898ea2b1c725e79dcb14ece4e1be0edf3d108
SwaraMansi <168108797+SwaraMansi@users.noreply.github.com>
2026-02-03 23:42:41 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.java
+++ 0072-edit-distance/0072-edit-distance.java
@@ -12,7 +12,7 @@ class Solution {
                 dp[i][j]= dp[i-1][j-1];
             }
             else{
-                dp[i][j]= Math.min(dp[i-1][j], Math.min(dp[i][j-1],dp[i][j]));
+                dp[i][j]= 1+ Math.min(dp[i-1][j], Math.min(dp[i][j-1],dp[i-1][j-1]));
             }
         }
      }
