c8fc12c13c2768f491d030798154eadc74592540
SwaraMansi <168108797+SwaraMansi@users.noreply.github.com>
2026-02-03 23:35:27 +0530

    Time: 20 ms (55.96%), Space: 54.4 MB (47.46%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.java
+++ 0115-distinct-subsequences/0115-distinct-subsequences.java
@@ -1,22 +1,22 @@
 class Solution {
     public int numDistinct(String s, String t) {
-        int n = s.length(), m = t.length();
-        int[][] dp = new int[n][m];
-        for (int[] row : dp) Arrays.fill(row, -1);
-        return helper(s, t, 0, 0, dp);
-    }
+     int n= s.length();
+     int m= t.length();     
+     int[][] dp=new int[n+1][m+1];
+     for(int i=0;i<=n;i++){
+      dp[i][0] = 1;
+     }
+     for(int i=1;i<=n;i++){
+        for(int j=1;j<=m;j++){
+            if(s.charAt(i-1)== t.charAt(j-1)){
+                dp[i][j]= dp[i-1][j-1] + dp[i-1][j];
 
-    private int helper(String s, String t, int i, int j, int[][] dp) {
-        if (j == t.length()) return 1;  
-        if (i == s.length()) return 0;    
-        if (dp[i][j] != -1) return dp[i][j];
-
-        long ways = 0;
-        if (s.charAt(i) == t.charAt(j)) {
-            ways += helper(s, t, i + 1, j + 1, dp);  
+            }
+            else{
+                dp[i][j]= dp[i-1][j];
+            }
         }
-        ways += helper(s, t, i + 1, j, dp);
-
-        return dp[i][j] = (int) ways;
+     }
+     return dp[n][m];  
     }
 }
