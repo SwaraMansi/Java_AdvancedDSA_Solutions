b97b1cee1d9dcf969d4ba1d11301e0a3697d663a
SwaraMansi <168108797+SwaraMansi@users.noreply.github.com>
2026-02-03 23:42:23 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.java
+++ 0072-edit-distance/0072-edit-distance.java
@@ -1,21 +1,21 @@
 class Solution {
-    public int minDistance(String word1, String word2) {
-        int m= word1.length();
-        int n= word2.length();
-        int[][] dp= new int[m][n];
-        for (int[] row : dp) Arrays.fill(row, -1);
-        return helper(m-1,n-1,word1,word2,dp);
-    }
-    public int helper(int i,int j,String p,String s, int[][] dp){
-       if (i < 0) return j + 1; 
-        if (j < 0) return i + 1;
-        if(dp[i][j]!=-1) return dp[i][j];
-        if(p.charAt(i)==s.charAt(j)){
-            dp[i][j]= 0+helper(i-1,j-1,p,s,dp);
+    public int minDistance(String s, String t) {
+        int n= s.length();
+     int m= t.length();     
+     int[][] dp=new int[n+1][m+1];
+     for(int i=0;i<=n;i++){
+      dp[i][0] = 1;
+     }
+     for(int i=1;i<=n;i++){
+        for(int j=1;j<=m;j++){
+            if(s.charAt(i-1)== t.charAt(j-1)){
+                dp[i][j]= dp[i-1][j-1];
+            }
+            else{
+                dp[i][j]= Math.min(dp[i-1][j], Math.min(dp[i][j-1],dp[i][j]));
+            }
         }
-        else{
-            dp[i][j]= 1+ Math.min(helper(i-1,j,p,s,dp) , Math.min(helper(i,j-1,p,s,dp), helper(i-1,j-1,p,s,dp)));
-        }
-        return dp[i][j];
+     }
+     return dp[n][m];  
     }
 }
