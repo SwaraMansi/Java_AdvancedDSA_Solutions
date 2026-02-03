65d88e1bc5e53a3cff7ec6a2a22fc5c19024262e
SwaraMansi <168108797+SwaraMansi@users.noreply.github.com>
2026-02-03 23:43:52 +0530

    Time: 5 ms (65.79%), Space: 47.1 MB (66.79%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.java
+++ 0072-edit-distance/0072-edit-distance.java
@@ -3,9 +3,12 @@ class Solution {
         int n= s.length();
      int m= t.length();     
      int[][] dp=new int[n+1][m+1];
-     for(int i=0;i<=n;i++){
-      dp[i][0] = 1;
-     }
+     for (int i = 0; i <= n; i++) {
+            dp[i][0] = i;  
+        }
+        for (int j = 0; j <= m; j++) {
+            dp[0][j] = j; 
+        }
      for(int i=1;i<=n;i++){
         for(int j=1;j<=m;j++){
             if(s.charAt(i-1)== t.charAt(j-1)){
