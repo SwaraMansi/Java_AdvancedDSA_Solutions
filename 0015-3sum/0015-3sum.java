a52fbf3844056e6446b32ff3194cbbfa7ca03a01
Swara Mansi Sinha <168108797+SwaraMansi@users.noreply.github.com>
2026-02-16 20:17:03 +0530

    Time: 34 ms (59.59%), Space: 59.3 MB (60.02%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,30 +1,26 @@
 class Solution {
-    public List<List<Integer>> threeSum(int[] nums) {
-          Arrays.sort(nums);
-          int n = nums.length;
-        List<List<Integer>> result = new ArrayList<>();
-            for (int j = 0; j < n ; j++) {
-                if (j > 0 && nums[j] == nums[j - 1]) continue;
-                int left = j + 1;
-                int right = n - 1;
-                while (left < right) {
-                    int sum = nums[j] + nums[left] + nums[right];
-                    if (sum == 0) {
-                        result.add(Arrays.asList(
-                        nums[j], nums[left], nums[right] ));
-                        left++;
-                        right--;
-                        while (left < right && nums[left] == nums[left - 1]) left++;
-                        while (left < right && nums[right] == nums[right + 1]) right--;
-                    }
-                    else if (sum < 0) {
-                        left++;
-                    }
-                    else {
-                        right--;
-                    }
+    public List<List<Integer>> threeSum(int[] arr) {
+      Arrays.sort(arr);
+      List<List<Integer>> res= new ArrayList<>();
+     int n= arr.length;
+     for(int i=0;i<n;i++){
+        if(i>0 && arr[i]==arr[i-1]) continue;
+           int left=i+1;
+        int right= n-1;
+        while(left<right){
+
+        int sum= arr[i]+arr[left]+arr[right];
+        if(sum==0){
+          res.add(Arrays.asList(arr[i],arr[left],arr[right]));
+        left++;
+        right--;
+         while (left < right && arr[left] == arr[left - 1]) left++;
+                    while (left < right && arr[right] == arr[right + 1]) right--;
                 }
+           else if (sum < 0) left++;
+                else right--;
             }
-        return result;
+        }
+     return res;
     }
 }
