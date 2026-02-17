231e0aaf42861f5a36bd08756e6482187e099267
Swara Mansi Sinha <168108797+SwaraMansi@users.noreply.github.com>
2026-02-17 14:30:59 +0530

    Time: 17 ms (30.89%), Space: 56.5 MB (55.85%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.java
+++ 0518-coin-change-ii/0518-coin-change-ii.java
@@ -1,24 +1,16 @@
 class Solution {
-    public int change(int target, int[] arr) {
-          int n = arr.length;
-        int[][] dp = new int[n + 1][target + 1];
-
-        // Base case
-        dp[0][0] = 1;
-
-        for (int i = 1; i <= n; i++) {
-            for (int t = 0; t <= target; t++) {
-
-                // Not take
-                dp[i][t] = dp[i - 1][t];
-
-                // Take (if possible)
-                if (arr[i - 1] <= t) {
-                    dp[i][t] = (dp[i][t] + dp[i][t - arr[i - 1]]);
-                }
-            }
+    public int change(int target,int[] arr) {
+        int n = arr.length;
+    int[][] dp= new int[n+1][target+1];
+    dp[0][0]=1;
+    for(int i=1;i<=n;i++){
+        for(int t=0; t<=target; t++){
+         dp[i][t]= dp[i-1][t];
+         if(arr[i-1]<=t){
+            dp[i][t]= (dp[i][t] + dp[i][t-arr[i-1]]);
+         }
         }
-
-        return dp[n][target];
+    }
+    return dp[n][target];
     }
 }
