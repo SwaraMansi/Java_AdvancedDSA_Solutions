3779c5f501412a87809c1358e953b3a1f2a222a1
Swara Mansi Sinha <168108797+SwaraMansi@users.noreply.github.com>
2026-03-25 17:50:49 +0530

    Time: 4 ms (97.2%), Space: 161.9 MB (26.57%) - LeetHub

--- 3546-equal-sum-grid-partition-i/3546-equal-sum-grid-partition-i.java
+++ 3546-equal-sum-grid-partition-i/3546-equal-sum-grid-partition-i.java
@@ -4,19 +4,16 @@ class Solution {
 
     long totalSum = 0;
 
-    // Step 1: total sum
     for (int[] row : grid) {
         for (int val : row) {
             totalSum += val;
         }
     }
 
-    // Step 2: check even
     if (totalSum % 2 != 0) return false;
 
     long target = totalSum / 2;
 
-    // Step 3: horizontal cut
     long currentSum = 0;
     for (int i = 0; i < m - 1; i++) {
         for (int j = 0; j < n; j++) {
@@ -25,7 +22,6 @@ class Solution {
         if (currentSum == target) return true;
     }
 
-    // Step 4: vertical cut
     currentSum = 0;
     for (int j = 0; j < n - 1; j++) {
         for (int i = 0; i < m; i++) {
